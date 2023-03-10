051a2af3c78410ba065e9a38fa4ec06a9a24f4df
lewisian8787 <124101304+lewisian8787@users.noreply.github.com>
2023-03-09 23:25:36 -0500

    Lesson 1 #101

---
+++ RB101/lesson_2/practice.rb
@@ -0,0 +1,7 @@
+def method(array)
+  new_array = array.join
+end
+array1 = ["I", "am", "a", "little", "fox"]
+puts array1
+result = method(array1)
+puts result
---
+++ RB101/lesson_2/pseudo_code.rb
@@ -0,0 +1,37 @@
+# a method that returns the sum of two integers
+
+# START
+# DEFINE method called sum_of_integers 
+# - must take TWO parameters both of which will be integers (num1 & num2)
+# within the method perform an operation that adds num1 to num 2 into a variable called result
+# END the decleration of the method
+
+# invoke the method with two numerical parameters
+# PRINT the outcome to the console using puts
+
+# END
+
+# a method that takes an array of strings, and returns a string that is all those strings concatenated together
+
+# START
+# DEFINE a method called string_concatenation 
+# - must take an array as a parameter
+# the method must convert the array into a concatenated string, most likely using the .join method
+# END the method definition
+# DEFINE a variable that is an array containing a series of seperated strings
+# CALL the method, passing through the newly created array as the only parameter and store the result in a variable named concatenated_string
+# PRINT concatenated_string to the console to check your result
+# END
+
+# a method that takes an array of integers, and returns a new array with every other element from the original array, starting with the first element. 
+
+# START
+# DEFINE an array that contains a series of integers
+# DEFINE an empty array to be used later
+# DEFINE a method that accepts one parameter that will be an array
+# within the method, invoke the each slice method, which will iterate over the parameter input and return the result into the previously declared array
+# END the method decleration
+# INVOKE the method passing through the original array as the only parameter. 
+# PRINT the new array to ensure it has worked as expected
+# END
+
