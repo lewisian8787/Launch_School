b4c18d704a2f188fe142ef39055c0a4e4f70080e
lewisian8787 <124101304+lewisian8787@users.noreply.github.com>
2023-03-27 20:00:51 -0400

    Calculator edits

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -3,15 +3,15 @@
 # perform the operation on the two numbers
 # output the result
 
-#answer = Kernel.gets
-#Kernel.puts answer
+# answer = Kernel.gets
+# Kernel.puts answer
 
 def prompt(message)
-  puts"=> #{message}"
+  puts "=> #{message}"
 end
 
 def valid_number?(num)
-  num.to_i() != 0
+  num.to_i.to_s == num
 end
 
 def operation_to_message(op)
@@ -42,68 +42,67 @@ end
 
 prompt("Hi #{name}.")
 
-loop do #main loop
+loop do # main loop
+  number1 = nil
+  loop do
+    prompt("What's the first number?")
+    number1 = gets.chomp
 
-number1 = nil
-loop do
-  prompt("What's the first number?")
-  number1 = gets.chomp
-
-  if valid_number?(number1)
-    break
-  else
-    prompt("Hmm.... that doens't look like a valid number.")
-  end 
-end
-
-number2 = nil
-
-loop do
-  prompt("What's the second number?")
-  number2 = gets.chomp
-
-  if valid_number?(number2)
-    break
-  else
-    prompt("Hmm.... that doens't look like a valid number.")
-  end 
-end
-
-#MSG could be any pair of matching words 
-
-operator_prompt = <<-MSG 
-  What operation would you like to perform?
-  1) add
-  2) subtract
-  3) multiply
-  4) divide
-MSG
-
-prompt(operator_prompt)
-
-operator = ''
-loop do
-  operator = gets.chomp
-
-  if %w(1 2 3 4).include?(operator)
-    break
-  else
-    prompt("Must choose 1, 2, 3 or 4")
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm.... that doens't look like a valid number.")
+    end
   end
-end
 
-prompt("#{operation_to_message(operator)} the two numbers...")
+  number2 = nil
 
-result = case operator
-          when '1'
-            number1.to_i + number2.to_i
-          when '2'
-            number1.to_i - number2.to_i
-          when '3'
-            number1.to_i * number2.to_i
-          when '4'
-            number1.to_f / number2.to_f 
-end
+  loop do
+    prompt("What's the second number?")
+    number2 = gets.chomp
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm.... that doens't look like a valid number.")
+    end
+  end
+
+  # MSG could be any pair of matching words
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = gets.chomp
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3 or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
 
   prompt("The result is #{result}.")
 
@@ -113,6 +112,3 @@ end
 end
 
 prompt("Thank you for using calculator.")
-
-
-
