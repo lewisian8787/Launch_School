f6e9ad9b020e917af2e3b437a84f2c023486d973
lewisian8787 <124101304+lewisian8787@users.noreply.github.com>
2023-03-31 12:45:59 -0400

    More updates

--- RB101/lesson_2/mortgage_calc.rb
+++ RB101/lesson_2/mortgage_calc.rb
@@ -2,6 +2,20 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def prompt_user_name # helper method to gather user name
+  name = ''
+  loop do
+    name = gets.chomp
+
+    if name.empty?()
+      prompt("Make sure to use a valid name.")
+    else
+      break
+    end
+  end
+  name
+end
+
 def valid_loan_length?(num) # helper method to validate loan length
   num.to_i.to_s == num
 end
@@ -20,28 +34,11 @@ def total_interest(monthly_payment, loan_length, loan_amount)
   ((monthly_payment * loan_length) - loan_amount).round(2)
 end
 
-prompt("Welcome to the Mortgage Calculator!" \
-       " This calculator will help you calculate your potential repayments" \
-       " before making any big decisions." \
-       " To begin, please enter your name:")
-
-name = ''
-loop do
-  name = gets.chomp
-
-  if name.empty?()
-    prompt("Make sure to use a valid name.")
-  else
-    break
-  end
-end
-
-prompt("Hello #{name}. Thank you for choosing Mortgage Calcultor.")
-
-loop do # outer loop
+def prompt_loan_amount # helper method to collect loan amount
   loan_amount = nil
-  loop do # inner loop for loan amount
-    prompt("What amount would you like to borrow? ($)")
+  loop do
+    prompt("What amount would you like to borrow?")
+    print '$'
     loan_amount = gets.chomp.gsub(',', '').gsub('$', '')
 
     if valid_loan_amount?(loan_amount)
@@ -51,55 +48,95 @@ loop do # outer loop
       " You can enter any amount between $1 and  $100,000.")
     end
   end
+  loan_amount
+end
 
+def prompt_loan_length # helper method to collect loan length
   loan_length = nil
-  loop do # inner loop for loan length
+  loop do
     prompt("Over how long would you like to borrow? (In months)")
     loan_length = gets.chomp
-
     if valid_loan_length?(loan_length)
       break
     else
       prompt("Hmm.... that amount is not valid. Please try again. " \
-             "Remember to enter in months, so 4 years would be entered as 48")
+            "Remember to enter in months, so 4 years would be entered as 48")
     end
   end
+  loan_length
+end
 
-  apr = nil
-  loop do # inner loop for APR rate
+def prompt_apr_amount # helper method to collect APR amount
+  apr_amount = nil
+  loop do
     prompt("What rate of APR would you like?")
     prompt("Example: For 10% type 10 or for 6.5% type 6.5")
-    apr = gets.chomp
-
-    if valid_apr?(apr)
+    apr_amount = gets.chomp.gsub('%', '')
+    if valid_apr?(apr_amount)
       break
     else
       prompt("Hmm.... that rate is not valid. Please try again. " \
-             "You can enter any amount from 0 to 100.")
+            "You can enter any amount from 0 to 100.")
     end
   end
+  apr_amount
+end
 
-  # original Launch School code m = p * (j / (1 - (1 + j)**(-n)))
-  monthly_interest_rate = (apr.to_f / 100) / 12 # convert to a float
+def calculate_monthly_payment(loan_amount, apr_amount, loan_length)
+  monthly_interest_rate = (apr_amount.to_f / 100) / 12 # convert to a float
   monthly_payment = loan_amount.to_f * (monthly_interest_rate /
-  (1 - (1 + monthly_interest_rate)**(-loan_length.to_i)))
-  rounded_monthly_payment = monthly_payment.round(2)
+    (1 - (1 + monthly_interest_rate)**(-loan_length.to_i)))
+  monthly_payment.round(2)
+end
+
+# main code
+
+prompt("Welcome to Mortgage Calculator!" \
+       " This calculator will help you calculate your potential repayments" \
+       " before making any big decisions." \
+       " To begin, please enter your name:")
+
+name = prompt_user_name()
+
+prompt("Hello #{name}. Thank you for choosing Mortgage Calcultor.")
+
+loop do
+  loan_amount = prompt_loan_amount()
+  system "clear"
+  prompt("You have entered $#{loan_amount}.")
+
+  loan_length = prompt_loan_length()
+  system "clear"
+  prompt("You have entered $#{loan_amount} over a period" \
+         " of #{loan_length} months.")
+
+  apr_amount = prompt_apr_amount()
+  system "clear"
+  prompt("You have entered $#{loan_amount}, over a period" \
+         " of #{loan_length} months," \
+        " at a rate of #{apr_amount}% APR.")
+
+  puts ">>> calculating repayments"
+  puts ">>> please wait"
+  puts ">>> almost there! "
+  puts ""
+
+  payment = calculate_monthly_payment(loan_amount, apr_amount, loan_length)
 
   prompt("Your loan amount would be $#{loan_amount}.")
 
   prompt("Your monthly repayments would be" \
-         " $#{rounded_monthly_payment} over a term of #{loan_length} months.")
+        " $#{payment} over a term of #{loan_length} months.")
 
   prompt("The total amount of interest you would pay would be $" \
-      "#{total_interest(monthly_payment, loan_length.to_i, loan_amount.to_f)}")
+      "#{total_interest(payment, loan_length.to_i, loan_amount.to_f)}")
 
-  prompt("Would you like to perform another calculation? (Y/N)")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  prompt("Would you like to try again? (Y/N)")
+  user_retry_response = gets.chomp
+  break unless user_retry_response.downcase.start_with?('y')
 end
 
-prompt("Are you finished using Mortgage Calculator? (Y/N)")
-answer = gets.chomp
-unless answer.downcase.start_with?('y')
-  prompt("Thank you for using the Mortgage Calculator. Goodbye!")
-end
+prompt("Thank you for using Mortgage Calculator. " \
+       "We hope to see you again soon.")
+
+# end of code
