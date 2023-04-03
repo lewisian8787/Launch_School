a425dc7ae25d03f47b6b4bf1557ff40dd3bae7cc
lewisian8787 <124101304+lewisian8787@users.noreply.github.com>
2023-04-02 20:42:35 -0400

    changes made to mortgage calc based on feedback

--- RB101/lesson_2/mortgage_calc.rb
+++ RB101/lesson_2/mortgage_calc.rb
@@ -2,133 +2,170 @@ require 'yaml'
 
 MESSAGES = YAML.load_file("mortgage_calc_messages.yml")
 
+# user input methods
+
 def prompt(message)
   puts "=> #{message}"
 end
 
-def prompt_user_name # helper method to gather user name
-  name = ''
-  loop do
-    name = gets.chomp
-
-    if name.empty?()
-      prompt(MESSAGES["name_empty"])
-    else
-      break
-    end
+def prompt_user_name
+  name = gets.chomp
+  while name.match?(/\A\s*\z/)
+    prompt(MESSAGES["name_empty"])
   end
+
   name
 end
 
-def valid_loan_length?(num) # helper method to validate loan length
-  num.to_i.to_s == num
-end
-
-def valid_loan_amount?(num) # verify number and loan amount
-  num.to_i.to_s == num && num.to_f >= 0 && num.to_f <= 100000
-end
-
-def valid_apr?(num)
-  num = num.gsub('%', '').strip # remove % sign and leading/trailing spaces
-  return false unless num.match?(/^\d+(\.\d+)?$/) # check for valid float format
-  num.to_f >= 0 && num.to_f <= 100 # check for valid APR range
-end
-
-def total_interest(monthly_payment, loan_length, loan_amount)
-  ((monthly_payment * loan_length) - loan_amount).round(2)
-end
-
-def prompt_loan_amount # helper method to collect loan amount
+def prompt_loan_amount
   loan_amount = nil
   loop do
     prompt(MESSAGES["loan_amount_question"])
     print '$'
     loan_amount = gets.chomp.gsub(',', '').gsub('$', '')
-
-    if valid_loan_amount?(loan_amount)
-      break
-    else
-      prompt(MESSAGES["invalid_loan_amount"])
-    end
+    break if valid_loan_amount?(loan_amount)
+    prompt(MESSAGES["invalid_loan_amount"])
   end
   loan_amount
 end
 
-def prompt_loan_length # helper method to collect loan length
+def prompt_loan_length
   loan_length = nil
   loop do
     prompt(MESSAGES["loan_length_question"])
     loan_length = gets.chomp
-    if valid_loan_length?(loan_length)
-      break
-    else
-      prompt(MESSAGES["invalid_loan_length"])
-    end
+    break if valid_loan_length?(loan_length)
+    prompt(MESSAGES["invalid_loan_length"])
   end
-  loan_length
+  loan_length.to_i
 end
 
-def prompt_apr_amount # helper method to collect APR amount
+def prompt_apr_amount
   apr_amount = nil
   loop do
     prompt(MESSAGES["apr_rate_question"])
     prompt(MESSAGES["apr_rate_question_help"])
     apr_amount = gets.chomp.gsub('%', '')
-    if valid_apr?(apr_amount)
-      break
-    else
-      prompt(MESSAGES["invalid_apr_rate"])
-    end
+    break if valid_apr?(apr_amount)
+    prompt(MESSAGES["invalid_apr_rate"])
   end
   apr_amount
 end
 
+# message user methods
+
+def welcome_message(name)
+  prompt("Hello #{name}. Thank you for choosing Mortgage Calcultor.")
+end
+
+def confirm_loan_amount(loan_amount)
+  system "clear"
+  prompt("You have entered $#{loan_amount}.")
+end
+
+def confirm_loan_length(loan_length, loan_amount)
+  system "clear"
+  prompt("You have entered $#{loan_amount} over a period" \
+  " of #{loan_length} months.")
+end
+
+def confirm_apr_amount(apr_amount, loan_length, loan_amount)
+  system "clear"
+  prompt("You have entered $#{loan_amount}, over a period" \
+         " of #{loan_length} months," \
+        " at a rate of #{apr_amount}% APR.")
+end
+
+def confirm_repayments(loan_amount, apr_amount, loan_length, payment)
+  prompt("For a loan of $#{loan_amount}, at #{apr_amount}% APR," \
+         " over a term of #{loan_length} months, your monthly repayments " \
+         "would be $#{payment}.")
+end
+
+def display_total_interest(payment, loan_length, loan_amount)
+  total_interest = total_interest(payment, loan_length.to_i, loan_amount.to_f)
+  prompt("The total interest you would pay would be $#{total_interest}")
+end
+
+# boolean methods
+
+def valid_loan_length?(num)
+  num.to_i.to_s == num && num.to_f >= 1 && num.to_f <= 120
+end
+
+def valid_loan_amount?(num)
+  num.to_i.to_s == num && num.to_f >= 100 && num.to_f <= 100000
+end
+
+def valid_apr?(num)
+  num = num.gsub('%', '').strip
+  return false unless num.match?(/^\d+(\.\d+)?$/)
+  num.to_f >= 0 && num.to_f <= 100
+end
+
+def should_try_again?
+  prompt(MESSAGES["try_again"])
+  user_retry_response = gets.chomp
+  user_retry_response.downcase.start_with?('y')
+end
+
+# mathmatical methods
+
+def total_interest(monthly_payment, loan_length, loan_amount)
+  ((monthly_payment * loan_length) - loan_amount).round(2)
+end
+
 def calculate_monthly_payment(loan_amount, apr_amount, loan_length)
-  monthly_interest_rate = (apr_amount.to_f / 100) / 12 # convert to a float
-  monthly_payment = loan_amount.to_f * (monthly_interest_rate /
-    (1 - (1 + monthly_interest_rate)**(-loan_length.to_i)))
-  monthly_payment.round(2)
+  loan_amount = loan_amount.to_f
+  apr_amount = apr_amount.to_f
+  monthly_interest_rate = apr_amount / (12 * 100)
+
+  if monthly_interest_rate == 0
+    loan_amount / loan_length
+  else
+    monthly_payment = loan_amount * (monthly_interest_rate /
+      (1 - (1 + monthly_interest_rate)**(-loan_length)))
+    monthly_payment.round(2)
+  end
+end
+
+# misscelaneous
+
+def calculation_loading_message
+  <<~HEREDOC
+    >>> Calculating repayments!
+    >>> Please wait!
+    >>> Almost there!
+    >>> ......
+  
+  HEREDOC
 end
 
 # main code
 
 prompt(MESSAGES["name_prompt_message"])
 name = prompt_user_name()
-
-prompt("Hello #{name}. Thank you for choosing Mortgage Calcultor.")
+welcome_message(name)
 
 loop do
   loan_amount = prompt_loan_amount()
-  system "clear"
-  prompt("You have entered $#{loan_amount}.")
+  confirm_loan_amount(loan_amount)
 
   loan_length = prompt_loan_length()
-  system "clear"
-  prompt("You have entered $#{loan_amount} over a period" \
-         " of #{loan_length} months.")
+  confirm_loan_length(loan_length, loan_amount)
 
   apr_amount = prompt_apr_amount()
-  system "clear"
-  prompt("You have entered $#{loan_amount}, over a period" \
-         " of #{loan_length} months," \
-        " at a rate of #{apr_amount}% APR.")
+  confirm_apr_amount(apr_amount, loan_length, loan_amount)
 
-  puts ">>> calculating repayments"
-  puts ">>> please wait"
-  puts ">>> almost there! "
-  puts ""
+  puts calculation_loading_message()
 
   payment = calculate_monthly_payment(loan_amount, apr_amount, loan_length)
 
-  prompt("With a loan of $#{loan_amount}, your monthly repayments would be" \
-        " $#{payment} over a term of #{loan_length} months.")
+  confirm_repayments(loan_amount, apr_amount, loan_length, payment)
 
-  prompt("The total amount of interest you would pay would be $" \
-      "#{total_interest(payment, loan_length.to_i, loan_amount.to_f)}")
+  display_total_interest(payment, loan_length, loan_amount)
 
-  prompt(MESSAGES["try_again"])
-  user_retry_response = gets.chomp
-  break unless user_retry_response.downcase.start_with?('y')
+  break unless should_try_again?
 end
 
 prompt(MESSAGES["goodbye"])
