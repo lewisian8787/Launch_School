df1fa51d8c5c09dffd4b9d9972df81ee9834882d
lewisian8787 <124101304+lewisian8787@users.noreply.github.com>
2023-04-27 08:12:58 -0400

    More work on easy 1 problems

---
+++ RB101/lesson_3/easy1_three.rb
@@ -0,0 +1,11 @@
+flintstones = ["Fred", "Wilma"]
+flintstones << ["Barney", "Betty"]
+flintstones << ["BamBam", "Pebbles"]
+
+p flintstones.flatten
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+array = flintstones.to_a[2]
+
+p array
--- RB101/lesson_3/easy1_two.rb
+++ RB101/lesson_3/easy1_two.rb
@@ -1,3 +1,21 @@
-('a'..'z').include? "cc" # false
+included = (10..100).include? 142
 
-is_42 = (10..100).include? 42
+puts included
+
+famous_words = "seven years ago..."
+
+famous_words.prepend("Four score and ")
+
+puts famous_words
+
+famous_words2 = "seven years ago..."
+
+famous_words2.insert(0, "Four score and ")
+
+puts famous_words2
+
+famous_words3 = "seven years ago..."
+
+famous_words3 = "Four score and " << famous_words3
+
+puts famous_words3
