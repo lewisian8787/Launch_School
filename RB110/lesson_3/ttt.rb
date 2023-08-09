9d4242d3ee900daa78f2a02a84446f3c7f8d70a0
lewisian8787 <124101304+lewisian8787@users.noreply.github.com>
2023-08-09 11:50:53 -0400

    ttt updates

--- RB110/lesson_3/ttt.rb
+++ RB110/lesson_3/ttt.rb
@@ -1,3 +1,9 @@
+require 'pry'
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
 def prompt(msg)
   puts "=> #{msg}"
 end
@@ -20,31 +26,46 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = ' '}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] == ' '}
+  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
 end
 
 def player_places_piece!(brd)
   square = ''
   loop do 
-    prompt "Choose a square (1-9):"
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
     square = gets.chomp.to_i
-    if empty_squares(brd).include?(square)
-      break
-    else
-      prompt "Sorry, not a valid choice."
-    end
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, not a valid choice."
   end
-    brd[square] = 'X'
+    brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  false
 end
 
 board = initialize_board
 display_board(board)
 
-player_places_piece!(board)
-puts board.inspect
+loop do 
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
+
 display_board(board)
