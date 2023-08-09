dbb985f33c28d6df0d195cefc63b2c73b7ad7439
lewisian8787 <124101304+lewisian8787@users.noreply.github.com>
2023-08-09 12:59:27 -0400

    ttt finished

--- RB110/lesson_3/ttt.rb
+++ RB110/lesson_3/ttt.rb
@@ -9,6 +9,9 @@ def prompt(msg)
 end
 
 def display_board(brd)
+  system 'clear'
+  puts "You're an #{PLAYER_MARKER}."
+  puts "Computer is #{COMPUTER_MARKER}."
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
@@ -55,17 +58,52 @@ def board_full?(brd)
 end
 
 def someone_won?(brd)
-  false
+  !!detect_winner(brd)
 end
 
-board = initialize_board
-display_board(board)
+def detect_winner(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + #rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + #columns
+                  [[1, 5, 9], [3, 5, 7]]              #diagonals
 
-loop do 
-  player_places_piece!(board)
-  computer_places_piece!(board)
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+       return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER &&
+      brd[line[1]] == COMPUTER_MARKER &&
+      brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+
+  loop do 
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
   display_board(board)
-  break if someone_won?(board) || board_full?(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Do you want to play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
-display_board(board)
+prompt "Thanks for playing!"
