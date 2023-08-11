47663539688d0ce3054638248eaddf2007300ade
lewisian8787 <124101304+lewisian8787@users.noreply.github.com>
2023-08-11 15:48:59 -0400

    ttt changes

--- RB110/lesson_3/ttt.rb
+++ RB110/lesson_3/ttt.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
-WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
-                [[1, 5, 9], [3, 5, 7]]              # diagonals
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
+                [[1, 5, 9], [3, 5, 7]]
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -11,21 +12,26 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
+def display_score(player_score, computer_score)
+  system 'clear'
+  prompt "Player Score: #{player_score}"
+  prompt "Computer Score: #{computer_score}"
+end
+
 # rubocop:disable Metrics/AbcSize
 def display_board(brd)
-  system 'clear'
-  puts "You're an #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+  puts "You're #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
   puts ""
   puts "     |     |"
-  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
-  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}  "
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
-  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
   puts "     |     |"
   puts ""
 end
@@ -44,18 +50,47 @@ end
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    prompt "Choose a square (#{joinor(empty_squares(brd))}}):"
     square = gets.chomp.to_i
-    break if empty_squares(brd).include?(square)
-    prompt "Sorry, not a valid choice."
+    if empty_squares(brd).include?(square)
+      break
+    else
+      prompt "Invalid input. Please choose an open square."
+    end
   end
+
   brd[square] = PLAYER_MARKER
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
-  brd[square] = COMPUTER_MARKER
+  move_counter = 0
+  WINNING_LINES.each do |line|
+    if brd.values_at(line[0], line[1]).count(PLAYER_MARKER) == 2
+      if empty_squares(brd).include?(line[2])
+        brd[line[2]] = COMPUTER_MARKER
+        move_counter += 1
+        break
+      end
+    elsif brd.values_at(line[1], line[2]).count(PLAYER_MARKER) == 2
+      if empty_squares(brd).include?(line[0])
+        brd[line[0]] = COMPUTER_MARKER
+        move_counter += 1
+        break
+      end
+    elsif brd.values_at(line[0], line[2]).count(PLAYER_MARKER) == 2
+      if empty_squares(brd).include?(line[1])
+        brd[line[1]] = COMPUTER_MARKER
+        move_counter += 1
+        break
+      end
+    end
+  end
+  if move_counter == 0
+    square = empty_squares(brd).sample
+    brd[square] = COMPUTER_MARKER
+  end
 end
+
 
 def board_full?(brd)
   empty_squares(brd).empty?
@@ -67,44 +102,71 @@ end
 
 def detect_winner(brd)
   WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
       return 'Player'
-    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+    elsif brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
   nil
 end
 
-def joinor(brd)
-  brd.each do |num| 
+def joinor(array, seperator = ', ', arg3 = 'or' )
+  if array.count <= 2
+    array.join(" #{arg3} ")
+  else
+    string = array.join(seperator)
+    string[-2] = " #{arg3} "
+    string
   end
 end
 
 loop do
-  board = initialize_board
-
+  computer_score = 0
+  player_score = 0
   loop do
+    board = initialize_board
+
+    loop do
+      display_score(player_score, computer_score)
+      display_board(board)
+
+      player_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+
+      computer_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+    end
+
     display_board(board)
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    system "clear"
+    if detect_winner(board) == 'Player'
+      player_score += 1
+    elsif detect_winner(board) == 'Computer'
+      computer_score += 1
+    end
 
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    display_score(player_score, computer_score)
+    display_board(board)
+
+    if someone_won?(board)
+      prompt "#{detect_winner(board)} won!"
+    else
+      prompt "It's a tie!"
+    end
+
+    if player_score == 5
+      break
+    elsif player_score == 5
+      break
+    end
+
   end
-
-  display_board(board)
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
-
-  prompt "Do you want to play again? (y or n)"
+  prompt "Play again? (y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt "Thanks for playing!"
+prompt "Thanks for playing Tic Tac Toe! Goodbye!"
