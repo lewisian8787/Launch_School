3113a4112556d28dedf73f7fd9f618ada4e27d5e
lewisian8787 <124101304+lewisian8787@users.noreply.github.com>
2023-08-19 00:38:54 -0400

    easy 3 work

--- small_problems/easy_3/arithmetic.rb
+++ small_problems/easy_3/arithmetic.rb
@@ -1,4 +1,47 @@
-#Write a program that prompts the user for two positive integers, and then prints the results of the 
-#following operations on those two numbers: addition, subtraction, product, quotient, remainder, and
+#Write a program that prompts the user for two positive integers, and 
+# then prints the results of the following operations on those two 
+# numbers: addition, subtraction, product, quotient, remainder, and
 #power. Do not worry about validating the input.
+
+def prompt(msg)
+  puts "==> #{msg}"
+end
+
+def addition(int, int2)
+  int + int2
+end
+
+def subtraction(int, int2)
+  int - int2
+end
+
+def product(int, int2)
+  int * int2
+end
+
+def quotient(int, int2)
+  int / int2
+end
+
+def remainder(int, int2)
+  int % int2
+end
+
+def power(int, int2)
+  int ** int2
+end
+
+prompt "Enter the first number:"
+first_number = gets.chomp.to_i
+
+prompt "Enter the second number:"
+second_number = gets.chomp.to_i
+
+prompt "#{first_number} + #{second_number} = #{addition(first_number, second_number)}"
+prompt "#{first_number} - #{second_number} = #{subtraction(first_number, second_number)}"
+prompt "#{first_number} * #{second_number} = #{product(first_number, second_number)}"
+prompt "#{first_number} / #{second_number} = #{quotient(first_number, second_number)}"
+prompt "#{first_number} % #{second_number} = #{remainder(first_number, second_number)}"
+prompt "#{first_number} ** #{second_number} = #{power(first_number, second_number)}"
+
 
