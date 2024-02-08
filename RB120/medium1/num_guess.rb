606dc99b20cc8a66397ee7f57e307f8e65c04ad1
Ian Lewis <124101304+lewisian8787@users.noreply.github.com>
2024-02-08 14:42:35 -0500

    Update num_guess.rb
    updated to include second question

--- RB120/medium1/num_guess.rb
+++ RB120/medium1/num_guess.rb
@@ -1,47 +1,80 @@
-require 'pry'
+#require 'pry'
+
 
 class GuessingGame
 
   attr_accessor :num_guesses, :user_response, :target_number
+  attr_reader :low_num, :high_num, :size_of_range
 
-  def initialize
-    @num_guesses = 7
-    @user_response = 0
-    @target_number = (1..100).to_a.sample
-  end 
+  def initialize(low_num, high_num)
+    @low_num = low_num
+    @high_num = high_num
+    @size_of_range = (high_num - low_num)
+  end
+
+  def clear
+    system 'cls'
+  end
 
   def welcome_message
-    puts ""
-    puts "You have #{num_guesses} guesses remaining."
-    puts "Enter a number between 1 and 100:"
+      puts ""
+      puts "Welcome to Mr. Lewis' number guessing game! You simply have to guess what number I am thinking of..."
+      puts ""
+        loop do
+          puts "Are you ready to play? Type 'yes' if so."
+          answer = gets.chomp
+          break if answer == 'yes'.downcase
+          puts "Sorry, that's an incorrect response. Please enter 'yes' if you are ready to play!"
+        end
   end
 
   def number_position
     return true if user_response == target_number
-    binding.pry
+
     if user_response < target_number
-      num_guesses -= 1
       puts "Your guess is too low..."
+      puts ""
+      puts "You have #{num_guesses} guesses remaining."
     elsif user_response > target_number
       puts "Your guess is too high..."
+      puts ""
+      puts "You have #{num_guesses} guesses remaining."
     end
+
     false
   end
 
+  def winning_message
+    puts "Congratulations! You WON!"
+  end
+  
+  def losing_message
+    puts "Sorry! Better luck next time!"
+    puts "The number was #{target_number}!"
+  end
+
   def play
+    @num_guesses = Math.log2(size_of_range).to_i + 1
+    @user_response = nil
+    @target_number = (low_num..high_num).to_a.sample
+    welcome_message
+    clear
     loop do
-      welcome_message
+      puts "Enter a number between #{low_num} and #{high_num}."
       loop do
-        user_response = gets.chomp.to_i
-        break unless !(1..100).include?(user_response) 
-        puts "Invalid guess. Enter a number between 1 and 100:"
+        @user_response = gets.chomp.to_i
+        clear
+        break if (low_num..high_num).include?(user_response) 
+        puts "Invalid guess. Enter a number between #{low_num} and #{high_num}:"
       end
+
       @num_guesses -= 1
-      number_position
-      break if @num_guesses == 0 || number_position
+      break if num_guesses == 0 || number_position
     end
+
+    user_response == target_number ? winning_message : losing_message
   end
 end
 
-game = GuessingGame.new
+game = GuessingGame.new(500, 1500)
 game.play
